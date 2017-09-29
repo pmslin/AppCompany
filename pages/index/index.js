270bdac2fdac011aeb8dd2fb9a0327ffe2d02c38
pmslin <344804202@qq.com>
2017-09-29 11:30:15 +0800

    app

---
+++ pages/index/index.js
@@ -0,0 +1,38 @@
+//index.js
+//获取应用实例
+var app = getApp()
+var util = require('../../utils/util');
+
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    imgalist: ['http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 2000,
+    duration: 1000
+  },
+
+
+
+  /**   
+   * 预览图片  
+   */
+  previewImage: function (e) {
+    var current = e.target.dataset.src;
+    wx.previewImage({
+      current: current, // 当前显示图片的http链接  
+      urls: this.data.imgalist // 需要预览的图片http链接列表  
+    })
+  }
+
+ 
+  
+})
