b92ccb5d6eb16702243ad6ea5b2e7c8c2f25bdbd
pmslin <344804202@qq.com>
2017-10-10 18:02:12 +0800

    modfy

--- pages/contact/contact.js
+++ pages/contact/contact.js
@@ -1,7 +1,7 @@
 Page({
   calling: function () {
     wx.makePhoneCall({
-      phoneNumber: '020-123456', 
+      phoneNumber: '020-123456',
       success: function () {
         console.log("拨打电话成功！")
       },
